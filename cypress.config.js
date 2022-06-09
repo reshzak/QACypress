439469cccf04544b68d883de9518f3e1f7763c92
reshzak123 <reshzak123@gmail.com>
2022-06-09 16:34:38 -0400

    Initial commit

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  QA: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress/e2e/TC01_basics.cy.js
@@ -0,0 +1,17 @@
+describe('Login', function(){
+    it('Sign in', function(){
+        /*cy.visit('https://react-redux.realworld.io/#login')
+        cy.get('input[type="email"]').type("reshzak123@gmail.com")
+        cy.get('input[type="password"]').type("abc")
+        cy.get('.btn').contains('Sign in').click()*/
+
+        cy.visit('https://www.commbank.com.au/home-loans/home-loan-calculators-and-tools.html')
+        cy.get('#calculators > .column-combo > .content-section > .item-section > :nth-child(2) > .item-inner > :nth-child(3) > .button_tertiary').click()
+        cy.get('#amount').clear().type('650000')
+        cy.get('#productId').select('3')
+        cy.get('#submit').click()
+        cy.get('.col-xs-12').should('be.visible').contains('Your principal and interest repayments would be $2,952 per month')
+    })
+
+  })
+  
