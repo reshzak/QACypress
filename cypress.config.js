e716a5a22d995727df95f041f3c0470344828b3e
reshzak123 <reshzak123@gmail.com>
2022-06-09 16:40:03 -0400

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  QA: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
